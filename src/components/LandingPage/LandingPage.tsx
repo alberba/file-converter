c3bb3a0b095d769430be159720e4b76a2640026b
alberba <83814585+alberba@users.noreply.github.com>
2025-10-13 09:39:46 +0200

    fix: Change to margin from padding in LandingPage to drop files outside the box

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -12,18 +12,20 @@ type LandingPageProps = {
 export default function LandingPage({ handleDrop }: LandingPageProps) {
   return (
     <div
-      className="landing-container mx-auto min-h-screen max-w-7xl p-2"
+      className="landing-container w-full min-h-screen py-2"
       onDrop={handleDrop}
       onDragOver={(ev) => ev.preventDefault()}
     >
-      <main className="mt-42 flex w-full flex-col items-center gap-32 text-center sm:gap-16">
-        <Hero fileInputcallable={handleDrop} />
-        <a href="#features-container">
-          <ArrowUp className="h-8 rotate-180 animate-bounce text-black" />
-        </a>
-        <Feature />
-      </main>
-      <Footer />
+      <div className="mx-auto max-w-5xl">
+        <main className="mt-42 flex w-full flex-col items-center gap-32 text-center sm:gap-16">
+          <Hero fileInputcallable={handleDrop} />
+          <a href="#features-container">
+            <ArrowUp className="h-8 rotate-180 animate-bounce text-black" />
+          </a>
+          <Feature />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
