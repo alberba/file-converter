bdcfa43380a2feb7f9a3e5ba6cac53978f96dac6
alberba <83814585+alberba@users.noreply.github.com>
2025-10-16 16:05:33 +0200

    Set Timeout break rule to hide again

--- src/components/MessageFadeIn.tsx
+++ src/components/MessageFadeIn.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import crossSvg from "@/assets/cross.svg";
+
 export default function MessageFadeIn() {
   const [show, setShow] = useState(false);
 
@@ -7,14 +9,32 @@ export default function MessageFadeIn() {
     const timer = setTimeout(() => {
       setShow(true);
     }, 4000);
-    return () => clearTimeout(timer);
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.code === "Space") {
+        clearTimeout(timer);
+        setShow(false);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener("keydown", handleKeyDown);
+    };
   }, []);
 
   return (
     <span
-      className={`fixed bottom-0 rounded-xl bg-black/50 px-2 py-1 font-semibold text-white transition-all duration-700 ${show ? "-translate-y-3 opacity-100" : "translate-y-12 opacity-0"}`}
+      className={`fixed bottom-0 z-10 rounded-xl bg-black/50 px-2 py-1 font-semibold text-white transition-all duration-700 ${show ? "-translate-y-3 opacity-100" : "translate-y-12 opacity-0"}`}
     >
-      Presiona o mantén el espacio para ver la imagen original
+      <span className="hidden md:inline">
+        Presiona o mantén el espacio para ver la imagen original
+      </span>
+      <span className="md:hidden flex items-center gap-2" onTouchStart={() => setShow(false)}>
+        Presiona la imagen para ver los cambios
+        <img src={crossSvg} alt="Boton para cerrar" className="h-4" />
+      </span>
     </span>
   );
 }
