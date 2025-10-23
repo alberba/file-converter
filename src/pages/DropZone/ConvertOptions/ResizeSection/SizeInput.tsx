54f5cbf967ad60b6d3801e7f4d123836cf9b0300
alberba <83814585+alberba@users.noreply.github.com>
2025-10-23 19:13:46 +0200

    test: agregar pruebas para el componente ResizeSection y su manejo de tamaño

--- src/pages/DropZone/ConvertOptions/ResizeSection/SizeInput.tsx
+++ src/pages/DropZone/ConvertOptions/ResizeSection/SizeInput.tsx
@@ -7,11 +7,14 @@ type SizeInputProps = {
 };
 
 export default function SizeInput({ type, value, setValue }: SizeInputProps) {
+  const id = `${type.toLowerCase()}-input`;
+
   return (
-    <InputBase title={type}>
+    <InputBase title={type} htmlFor={id}>
       <input
         className="w-full rounded-md bg-gray-200 p-1 px-3 text-sm"
         type="number"
+        id={id}
         value={value || 0}
         onChange={(e) => setValue(Number(e.target.value))}
       />
