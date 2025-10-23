8d1ab09d5639f67a9290ba34e6be10f9e4e889cc
alberba <83814585+alberba@users.noreply.github.com>
2025-10-23 18:11:23 +0200

    test: refactor pruebas del componente ConvertOptions para mejorar la legibilidad y la gestión de estados

--- src/pages/DropZone/ConvertOptions/ConvertOptions.test.tsx
+++ src/pages/DropZone/ConvertOptions/ConvertOptions.test.tsx
@@ -5,21 +5,36 @@ import {
   createMockFileData,
 } from "@/testUtils/mocks";
 import { vi } from "vitest";
+import * as imageUtils from "@/utils/imageUtils";
 
 describe("ConvertOptions Component", () => {
-  it("should toggle options visibility when header is clicked", () => {
-    const file = createMockFileData();
-    const options = createMockConversionOptions();
+  const file = createMockFileData();
+  const options = createMockConversionOptions();
+  const onOptionsChange = vi.fn();
+  const convertedImageBlob = new Blob(["mock image data"], {
+    type: "image/png",
+  });
 
+  beforeEach(() => {
     render(
       <ConvertOptions
         file={file}
         options={options}
-        onOptionsChange={vi.fn()}
-        convertedImageBlob={new Blob()}
+        onOptionsChange={onOptionsChange}
+        convertedImageBlob={convertedImageBlob}
       />,
     );
+  });
 
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should render without crashing", () => {
+    expect(screen.getByRole("heading", { level: 2 })).toBeInTheDocument();
+  });
+
+  it("should toggle options visibility when header is clicked", () => {
     const convertOptionsHeader = screen.getByTestId("convert-options-header");
     const convertOptionsContent = screen.getByTestId("convert-options-content");
 
@@ -29,5 +44,24 @@ describe("ConvertOptions Component", () => {
     fireEvent.click(convertOptionsHeader);
 
     expect(convertOptionsContent).toHaveClass("opacity-0");
+  });
+
+  it("should call onOptionsChange when options are changed", () => {
+    const formatSelector = screen.getByRole("combobox");
+    fireEvent.change(formatSelector, { target: { value: "jpeg" } });
+
+    expect(onOptionsChange).toHaveBeenCalledWith({
+      ...options,
+      selectedFormat: "jpeg",
+    });
+  });
+
+  it("should call downloader function when download button is clicked", () => {
+    const downloadImageSpy = vi.spyOn(imageUtils, "downloadImage");
+    const downloadButton = screen.getByTestId("convert-button-badge");
+    fireEvent.click(downloadButton);
+
+    expect(downloadImageSpy).toHaveBeenCalled();
+    downloadImageSpy.mockRestore();
   });
 });
